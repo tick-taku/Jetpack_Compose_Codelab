ffe277221934372cccf4f57036a6ecd66b7458ef
tick.taku.77 <tick.taku.77@gmail.com>
2020-09-04 18:03:45 +0900

    Show text list.

--- app/src/main/java/com/example/jetpackcompose/codelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/codelab/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.jetpackcompose.codelab
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.InnerPadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.example.jetpackcompose.codelab.ui.JetpackComposeCodelabTheme
 
@@ -17,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             JetpackComposeCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting((0 until 100).map { it.toString() })
                 }
             }
         }
@@ -25,14 +33,20 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(text: List<String>) {
+    LazyColumnFor(text, contentPadding = InnerPadding(8.dp)) {
+        Card(modifier = Modifier.fillParentMaxWidth().padding(6.dp)) {
+            Text(text = it,
+                 textAlign = TextAlign.Start,
+                 modifier = Modifier.fillMaxWidth().padding(30.dp, 16.dp, 16.dp, 16.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeCodelabTheme {
-        Greeting("Android")
+        Greeting((0 until 10).map { it.toString() })
     }
 }
